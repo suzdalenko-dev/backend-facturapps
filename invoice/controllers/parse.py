6c1324d2f7c19500842d8e7bcbe8b8596af3fae8
suzdalenko-dev <alexey.saron@gmail.com>
2024-12-08 00:39:42 +0100

    suzdalenko

--- invoice/controllers/parse.py
+++ invoice/controllers/parse.py
@@ -1,5 +1,10 @@
 from invoice.utils.util_suzdal import json_suzdal
+import requests
 
 
 def parse_work(request, id):
-    return json_suzdal({'id': id,})
+
+    url = "https://example.com"
+    response = requests.get(url)
+
+    return json_suzdal({'id': response.text })
